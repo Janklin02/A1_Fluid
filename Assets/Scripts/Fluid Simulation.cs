e3138ce4b2f152e09f900da1acdbe35a745508ce
Oscar <mroscar15@gmail.com>
2026-08-06 18:18:03 +1000

    begining uuughhh

---
+++ Assets/Scripts/Fluid Simulation.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FluidSimulation : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        DrawCircle(Vector2.zero, 1, Color.cyan);
+    }
+}
